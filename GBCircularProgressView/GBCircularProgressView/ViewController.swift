dd1a2e6edd0200dcae634abf4c8acbec7c2293c8
Guillaume Bourachot <g.bourachot@kreactive.com>
2018-03-26 09:26:06 +0200

    Implementation of my custom circular progressView. To use it, you just have to set the progress variable from 0 to 1

---
+++ GBCircularProgressView/GBCircularProgressView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GBCircularProgressView
+//
+//  Created by Guillaume Bourachot on 26/03/2018.
+//  Copyright © 2018 GBourachot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
